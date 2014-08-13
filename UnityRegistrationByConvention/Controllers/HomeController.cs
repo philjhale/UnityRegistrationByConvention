8196d04fc95ca31da77eca07033f67496f1f3ec6
Phil Hale <philip.hale@tombola.com>
2014-08-13 13:57:35 +0100

    Switched to using Unity.MVC4 package because it's easily and added a manual registration to prove it's working

--- UnityRegistrationByConvention/Controllers/HomeController.cs
+++ UnityRegistrationByConvention/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Services;
 
 namespace UnityRegistrationByConvention.Controllers
 {
 	public class HomeController : Controller
 	{
+		public HomeController(IUserService userService)
+		{
+		}
+
 		public ActionResult Index()
 		{
 			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
